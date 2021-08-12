3fe64ca69a06493a73af5faa8edc5760bf0c2233
stercoris <dimarodin0@gmail.com>
2021-08-12 11:09:11 +0400

    support for buttonS fragments

--- src/core/factory/elements/row/parser.ts
+++ src/core/factory/elements/row/parser.ts
@@ -5,5 +5,6 @@ export type Row = Button[];
 export type RowParser = (name: "row", props: null, ...children: Row) => Row;
 
 export const ParseRow: RowParser = (_, __, ...children) => {
-  return children;
+  const buttons = children.flat();
+  return buttons;
 };
