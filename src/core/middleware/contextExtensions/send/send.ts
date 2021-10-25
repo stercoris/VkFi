284a067e2d05f9981d3869c8590ac8c3f4b44465
Dmitriy Rodin <dimarodin0@gmail.com>
2021-10-26 00:47:48 +0400

    fix(send): unused non-existing import

--- src/core/middleware/contextExtensions/send/send.ts
+++ src/core/middleware/contextExtensions/send/send.ts
@@ -1,6 +1,5 @@
 import { BuildKeyboard } from "core/builder/IBuilder";
 import { ContextBundle } from "core/middleware/IContextBundle";
-import { nodeToVkIoKeyboard, unpackContent } from "R1IO";
 import { IMessageContextSendOptions } from "vk-io";
 
 export const applyCustomSend = <C extends {}>(
